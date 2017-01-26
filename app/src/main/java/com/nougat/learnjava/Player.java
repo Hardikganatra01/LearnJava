9c5fa1a589c542385c8b88b385f3c2116a932eeb
hardikganatra01 <hardikganatra01@gmail.com>
2017-01-26 18:32:58 +0530

    Classes

---
+++ app/src/main/java/com/nougat/learnjava/Player.java
@@ -0,0 +1,29 @@
+package com.nougat.learnjava;
+
+/**
+ * Created by Bhavya Thacker on 26-01-2017.
+ */
+
+public class Player {
+    private String handleName;
+    private int lives;
+    private int level;
+    private     int score;
+
+    public Player(){
+        handleName = "Unknown Player";
+        lives = 3;
+        level = 1;
+        score = 0;
+    }
+    public String getHandleName(){
+        return handleName;
+    }
+
+    public void setHandleName(String handle){
+        if (handle.length() <3){
+            return;
+        }
+        handleName = handle;
+    }
+}
