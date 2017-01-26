380c449699b680428a63f6dbfda096d307adc54d
hardikganatra01 <hardikganatra01@gmail.com>
2017-01-26 16:20:30 +0530

    practising if and boolean

--- app/src/main/java/com/nougat/learnjava/HelloWorld.java
+++ app/src/main/java/com/nougat/learnjava/HelloWorld.java
@@ -6,27 +6,12 @@ package com.nougat.learnjava;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("My first Java program");
+        int lives = 0;
 
-        String tim;
-        tim = "Tim Buchalka";
-        System.out.println(tim);
-
-        int timWeeklySalary = 32;
-        int timsMonthlySalary = timWeeklySalary * 4;
-        System.out.println("Weekly salary: " + timWeeklySalary);
-        System.out.println("Monthly salary: " + timsMonthlySalary);
-
-        int apples = 6;
-        int oranges = 5;
-        int fruit = apples - oranges;
-        System.out.println("I have " + fruit + " fruit");
-
-        int weeks = 130;
-        double years = weeks / 52.0;
-        System.out.println(weeks + " weeks is " + years + " years");
-
-        System.out.println("Hello, " + tim + " here");
+        if (lives < 1){
+            System.out.println("Game over!");
+        }else {
+            System.out.println("You're still alive!");
+        }
     }
 }
